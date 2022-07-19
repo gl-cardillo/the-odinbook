d022dbd57854b417906a125430062fed2c5d415d
gl-cardillo <glucacardillo94@gmail.com>
2022-07-19 12:25:18 +0200

    fix middleware

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,9 +1,13 @@
+const jwt = require("jsonwebtoken");
+
 function verifyToken(req, res, next) {
   const bearerHeader = req.headers["authorization"];
   if (typeof bearerHeader !== "undefined") {
     const token = bearerHeader.split(" ")[1];
     if (token != "undefined" && token !== "null") {
-      req.token = token;
+      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      req.user = decoded;
+      console.log(decoded)
       next();
     }
   } else {

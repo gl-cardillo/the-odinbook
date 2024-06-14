49fed5b8fbd0caee79bca9e03f1ddee7b61733b6
luca-cardi <giovanniluca.cardillo@gmail.com>
2024-06-15 00:08:51 +0100

    refractor code

--- server/index.js
+++ server/index.js
@@ -1,38 +1,38 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const path = require("path");
 const logger = require("morgan");
 const compression = require("compression");
 const helmet = require("helmet");
 const cors = require("cors");
-const passport = require("passport");
 require("dotenv").config();
-require("./server/config/mongoDB");
-require("./server/config/passport");
+require("./config/mongoDB");
+require("./config/passport");
 
-const usersRouter = require("./server/routes/user");
-const authRouter = require("./server/routes/auth");
-const postRouter = require("./server/routes/post");
-const commentRouter = require("./server/routes/comment");
+const usersRouter = require("./routes/user");
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+const commentRouter = require("./routes/comment");
 
 const app = express();
 
 app.use(cors());
-
+app.use(helmet());
 app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use("/auth", authRouter);
 app.use("/user", usersRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
 });
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
 
 module.exports = app;
